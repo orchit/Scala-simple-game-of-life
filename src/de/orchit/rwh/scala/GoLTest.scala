6db85e440a36c717385a078beefd2a107a461d9a
pcornelissen <cornelis@pcornelissen.de>
2013-06-10 11:40:53 +0200

    9 green REFACTOR Class to Object

--- src/de/orchit/rwh/scala/GoLTest.scala
+++ src/de/orchit/rwh/scala/GoLTest.scala
@@ -4,10 +4,10 @@ import org.scalatest.FunSuite
 
 
 class GoLTest extends FunSuite{
-  class DeadCell{val isDead=true}
+  object DeadCell{val isDead=true}
 
   test("we have a dead cell"){
-    val cell = new DeadCell
-    assert(cell isDead,"Cell is not dead")
+    assert(DeadCell isDead,"Cell is not dead")
   }
+
 }
