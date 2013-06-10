b595bf392d6db78efa9da67329ed79fe83d32ec9
pcornelissen <cornelis@pcornelissen.de>
2013-06-10 13:17:48 +0200

    20 green a living cell stays alive with 2 neighbors

--- src/de/orchit/rwh/scala/GoLTest.scala
+++ src/de/orchit/rwh/scala/GoLTest.scala
@@ -3,30 +3,41 @@ package de.orchit.rwh.scala
 import org.scalatest.FunSuite
 
 
-class GoLTest extends FunSuite{
-  object DeadCell{val isDead=true}
-  object LivingCell{val isDead=false
-  def nextState(neighbors:Int) = DeadCell
+class GoLTest extends FunSuite {
+  trait Cell{
+    def isDead:Boolean
+  }
+  object DeadCell extends Cell{
+    def isDead = true
   }
 
-  test("we have a dead cell"){
-    assert(DeadCell isDead,"Cell is not dead")
+  object LivingCell extends Cell{
+    def isDead = false
+
+    def nextState(neighbors: Int) = {
+        if(neighbors<2) DeadCell
+        else LivingCell
+    }
   }
 
-  test("we can have a living cell"){
+  test("we have a dead cell") {
+    assert(DeadCell isDead, "Cell is not dead")
+  }
+
+  test("we can have a living cell") {
     assert(LivingCell.isDead === false, "Cell is not living")
   }
 
-  test("a living cell dies when it has no neighbor"){
-    assert(LivingCell.nextState(0).isDead,"Cell should have died of loneliness")
+  test("a living cell dies when it has no neighbor") {
+    assert(LivingCell.nextState(0).isDead, "Cell should have died of loneliness")
   }
 
-  test("a living cell dies when it has one neighbor"){
-    assert(LivingCell.nextState(1).isDead,"Cell should have died of loneliness")
+  test("a living cell dies when it has one neighbor") {
+    assert(LivingCell.nextState(1).isDead, "Cell should have died of loneliness")
   }
 
-  test("a living cell stays alive when it has two neighbors"){
-    assert(LivingCell.nextState(2).isDead === false,"Cell should have died of loneliness")
+  test("a living cell stays alive when it has two neighbors") {
+    assert(LivingCell.nextState(2).isDead === false, "Cell should have died of loneliness")
   }
 
 
